3b20c4f2746d2d0c9706107f50c607cc0bb0a828
unknown <chombobintiomar5gmail.com>
2025-05-15 17:07:32 +0300

    first commit

---
+++ intro_class.py
@@ -0,0 +1,19 @@
+print('Binti Omar')
+print (100)
+print(100)
+print(1000)
+x=20
+y=5
+z=x+y
+print (z)
+my_student='alex'
+print(my_student)
+# calculate area of a square
+side=20
+area=side*side
+print(area)
+# calculate area of a rectangle
+length=10
+width=5
+area=length*width
+print(area)
