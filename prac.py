ae25b58213cc73dcc800976917d8fdede6a5f785
unknown <chombobintiomar5gmail.com>
2025-05-15 19:43:48 +0300

    first commit

---
+++ prac.py
@@ -0,0 +1,10 @@
+print('Zarifah')
+print('20 years')
+my_mom='siti'
+my_brother='adam'
+print(my_mom)
+print(my_brother)
+# calculate area of a circle
+radius=5
+area=3.14*radius*radius
+print(area)
