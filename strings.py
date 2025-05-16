8d91fe5157faf4c983aa47c35ad53779e34b3eb7
unknown <chombobintiomar5gmail.com>
2025-05-16 16:57:15 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,29 @@
+first_name="Binti"
+second_name="Omar"
+print(type(second_name))
+y=99.91
+print(type(y))
+z=False
+print(type(z))
+x=100
+print(type(x))
+full_name=first_name+" "+second_name
+print(full_name)
+x='1000'
+y='2000'
+z=x+y
+print(z)
+x='1000'
+y='2000'
+z=x+y
+print(z)
+text="python programming"
+print(text[10])
+print(text[7:18])
+print(text[0:6])
+# string methods
+my_text="HEllo"
+my_text=my_text.capitalize()
+print(my_text)
+my_text=my_text .count('l')
+print(my_text)
