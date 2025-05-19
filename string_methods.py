969961eb188b823283e0b28de85dd87d1511a582
unknown <chombobintiomar5gmail.com>
2025-05-19 17:00:20 +0300

    first commit

---
+++ string_methods.py
@@ -0,0 +1,16 @@
+text="python class"
+text_cap=text.capitalize()
+print(text_cap)
+print(text.count('o'))
+
+c=" learn python "
+c=c.strip()
+print(c)
+c_tit=c.title()
+print(c_tit)
+g="hello"
+print(g[::-1])
+text=" learn python "
+text=text.replace(' ','_')
+print(text)
+print(text[10:13])
--- stringtask.py
+++ stringtask.py
@@ -1,7 +1,42 @@
-name="JOHn"
+name=" JOHn ."
 x=name.lower()
+x=x.replace('.',' ')
+x=x.strip()
 print(x)
 text="The Dog Breed is German Shepherd"
 print(text[8:24])
 text="Defeats for the Clinton forces,this was her moment of triumph"
 print(text[16:30])
+sentence="the lazy dog;ran so fast;it hit the wall."
+text=sentence.split(';')
+print(text)
+print(len(text))
+r='["E","W","C"]'
+z='E'+'W'+'C'
+print(z)
+first_name=" joh.n"
+first_name=first_name.replace('.','')
+first_name=first_name.strip()
+print(first_name)
+last_name='Do,e'
+last_name=last_name.replace(',','')
+print(last_name)
+full_name=first_name+" "+last_name
+print(full_name)
+
+#   Create a new python file stringtask.py and attempt the 5 questions below:
+# Clean up the following variable to give the clean version in lower case. Using inbuilt methods in the str class 
+# name = “  JOHn  .“ to “john”
+# Slice the below string to get you the resulting sentence:
+# sentence_one = “The Dog Breed is German Shepherd” only display “Breed is German”
+# sentence_two = “Defeats for the Clinton forces, this was her moment of triumph” only display “Clinton forces”
+# Split the below sentence using a semicolon i.e ; And display length of the result. 
+# “The lazy dog; ran so fast; it hit the wall.” 
+# first_name="  Joh.n"  last_name="   Do,e" Clean up and display Full name i.e John Doe
+
+# Having the string r = '["E","W","C"]' #Manipulate it to display EWC
+# Attempt questions in the link below. Whether you get the right answer or not, still read the solution explanation.
+# https://realpython.com/quizzes/python-strings/
+
+
+           
