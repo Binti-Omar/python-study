a95222edcba8edfa6acd527deee4c016a9edf5f4
unknown <chombobintiomar5gmail.com>
2025-05-23 16:36:56 +0300

    dict

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+student={'name':'alex',
+         'age':30,
+         'email':'alex@gmail.com'}
+print(type(student))
+student={'name':'alex',
+         'age':30,
+         'email':'alex@gmail.com',}
+print(student)
+# assessing
+student['age']=40
+print(student)
+student['occupation']='doctor'
+print(student)
+student['phone']='0717824020'
+print(student)
+student['location']={'town':'nakuru','address':1000,'street':'kimathi'}
+print(student['location']['street'])
